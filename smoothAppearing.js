47612c9a23e4d5c6d30188c34c7e37c3d856dc6e
evilHoms <homspage@gmail.com>
2018-02-13 15:47:44 +0300

    min version added

--- smoothAppearing.js
+++ smoothAppearing.js
@@ -1,5 +1,15 @@
 'use strict';
 
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  console.info('polyfill for IE11');
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 class SmoothAppearing {
   constructor() {
     this.animationSpeed = '1s';
